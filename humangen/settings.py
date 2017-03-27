28826d1b70267842f6a5d1d3b0cef2ed55e25781
Nick Chapman <nick@nchapman.io>
2017-03-27 03:40:12 +0000

    Starting amr app

--- humangen/settings.py
+++ humangen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7fdv_g&&kxcy@yg&ifofv77gi_=md)aon-*rx)fnllf4b%z#io'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['amr.nchapman.io']
 
 
 # Application definition
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'humangen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+	'OPTIONS': {
+		'read_default_file': '/home/nick/humangen/humangen/my.cnf',
+	},
     }
 }
 
@@ -118,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
