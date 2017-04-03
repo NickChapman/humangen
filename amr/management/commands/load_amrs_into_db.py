c4200054cb3f1e1628c12ed9ade5d7d34815c358
Nick Chapman <nlc35@georgetown.edu>
2017-04-03 16:26:32 -0400

    Loading AMRs moved into management command

--- amr/management/commands/load_amrs_into_db.py
+++ amr/management/commands/load_amrs_into_db.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from amr.models import AmrEntry
+import re
 
 class Command(BaseCommand):
     help = 'Reads the AMRs out of the specified AMR file and puts them into the database'
@@ -8,4 +9,34 @@ class Command(BaseCommand):
         parser.add_argument('filename')
 
     def handle(self, *args, **options):
-        print(options['filename'])
+        filename = options['filename']
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+            # Remove blank lines and comment lines
+            lines = [line.strip() for line in lines if line.strip() != '' and not line.startswith('#')]
+            # Now gather all of the entries
+            iLine = 0
+            sentence_number_pattern = re.compile(r'^[0-9]+\.\s+')
+            end_of_sentence_number_pattern = re.compile(r'\(lpp_1943\.[0-9]+\)')
+            while iLine < len(lines):
+                while iLine < len(lines) and not len(sentence_number_pattern.findall(lines[iLine])):
+                    iLine += 1
+                # When it halts we have found the sentence
+                # Strip off the number at the beginning and end
+
+                #    sed - r
+                #    's/^[0-9]+\.//g' | sed - r
+                #    's/\(lpp_1943\.[0-9]+\)//g'
+                sentence = sentence_number_pattern.sub('', lines[iLine])
+                sentence = end_of_sentence_number_pattern.sub('', sentence)
+                iLine += 1
+                # Now get the AMR that is under the sentence
+                # Iterate while a sentence number is not present
+                amr_lines = []
+                while iLine < len(lines) and not len(sentence_number_pattern.findall(lines[iLine])):
+                    if lines[iLine].strip() != '':
+                        amr_lines.append(lines[iLine])
+                    iLine += 1
+                amr = '\n'.join(amr_lines)
+                amr_entry = AmrEntry(sentence=sentence, amr=amr)
+                amr_entry.save()
